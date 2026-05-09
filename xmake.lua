e2c7a205cc0b862a773013bacdfec4370e387e94
anonyein <48914448+anonyein@users.noreply.github.com>
2026-05-10 00:16:19 +0800

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -3,6 +3,9 @@ set_project("isolated-vm")
 set_version("0.0.1")
 add_rules("mode.release")
 
+-- 启用 C++20 模块支持
+set_policy("build.c++.modules", true)
+
 set_languages("gnu++26")
 add_defines("EXPORT_IS_EXPORT")
 
@@ -26,62 +29,60 @@ end
 
 add_cxxflags("-fvisibility=hidden", "-fPIC")
 
--- 排除规则：递归删除 node_modules 和 test 目录下的文件
-function exclude_extra()
-    remove_files("packages/*/node_modules/**")
+-- 排除规则：仅删除 test/ 目录下的文件，保留 node_modules（项目包链接）
+function exclude_tests()
     remove_files("packages/*/test/**")
     remove_files("packages/*/tests/**")
 end
 
--- 每个目标添加完文件后调用排除函数
 target("utility")
     set_kind("static")
     add_files("packages/utility/**.cc")
-    exclude_extra()
+    exclude_tests()
 
 target("auto_js")
     set_kind("static")
     add_deps("utility")
     add_files("packages/auto_js/js/**.cc")
-    exclude_extra()
+    exclude_tests()
 
 target("nodejs")
     set_kind("static")
     add_deps("auto_js")
     add_defines("NAPI_VERSION=10")
     add_files("packages/third_party/nodejs/**.cc")
-    exclude_extra()
+    exclude_tests()
 
 target("nodejs_v8")
     set_kind("static")
     add_deps("auto_js")
     add_files("packages/third_party/v8/**.cc")
-    exclude_extra()
+    exclude_tests()
 
 target("napi_js")
     set_kind("static")
     add_deps("utility", "auto_js", "nodejs", "nodejs_v8")
     add_files("packages/auto_js/napi/**.cc")
-    exclude_extra()
+    exclude_tests()
 
 target("v8_js")
     set_kind("static")
     add_deps("utility", "auto_js", "nodejs_v8")
     add_files("packages/auto_js/v8/**.cc")
-    exclude_extra()
+    exclude_tests()
 
 target("isolated_vm")
     set_kind("static")
     add_deps("utility", "v8_js", "napi_js")
     add_files("packages/isolated-vm/addon/**.cc")
     add_files("packages/backend_napi_v8/api/**.cc")
-    exclude_extra()
+    exclude_tests()
 
 target("backend_napi_v8")
     set_kind("shared")
     add_deps("utility", "auto_js", "nodejs", "nodejs_v8", "napi_js", "v8_js", "isolated_vm")
     add_files("packages/backend_napi_v8/**.cc")
-    exclude_extra()
+    exclude_tests()
     add_links("javet-node-android-i18n")
     if is_plat("android") then
         add_links("uv", "android", "log", "EGL", "GLESv2", "OpenSLES")
