3a358b229a176c813d52cb42fd051caef17e6e38
anonyein <48914448+anonyein@users.noreply.github.com>
2026-05-10 14:49:09 +0800

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -53,11 +53,15 @@ end
 target("utility")
     set_kind("static")
     add_files("packages/utility/**.cc", {filter = filter_files})
+    -- 公开模块接口，保证所有依赖方都能使用 util
+    set_policy("build.c++.modules.public", true)
 
 target("auto_js")
     set_kind("static")
     add_deps("utility")
     add_files("packages/auto_js/js/**.cc", {filter = filter_files})
+    -- 公开 auto_js 模块接口给 isolated_vm 等
+    set_policy("build.c++.modules.public", true)
 
 target("nodejs")
     set_kind("static")
@@ -73,21 +77,26 @@ target("nodejs_v8")
     set_kind("static")
     add_deps("auto_js")
     add_files("packages/third_party/v8/v8.cc")
+    -- 公开 v8 模块接口
+    set_policy("build.c++.modules.public", true)
 
 target("napi_js")
     set_kind("static")
     add_deps("utility", "auto_js", "nodejs", "nodejs_v8")
     add_files("packages/auto_js/napi/**.cc", {filter = filter_files})
     add_includedirs(path.join(workspace, "packages/auto_js/napi/include"), {public = true})
+    -- 公开 napi_js 模块接口
+    set_policy("build.c++.modules.public", true)
 
 target("v8_js")
     set_kind("static")
     add_deps("utility", "auto_js", "nodejs_v8")
     add_files("packages/auto_js/v8/**.cc", {filter = filter_files})
+    -- 公开 v8_js 模块接口
+    set_policy("build.c++.modules.public", true)
 
 target("isolated_vm")
     set_kind("static")
-    -- 显式依赖 auto_js，解决 missing auto_js dependency 错误
     add_deps("utility", "auto_js", "v8_js", "napi_js", "nodejs_v8")
     add_files("packages/isolated-vm/addon/**.cc", {filter = filter_files})
     add_files("packages/backend_napi_v8/api/**.cc", {filter = filter_files})
